b8ccefda0651a6d412ba6eff9c70f5b13d664a88
fttmatteo <valenciaardila988@icloud.com>
2026-05-29 01:03:46 -0500

    chore: add eslint-disable for useMaps export in MapsContext

--- src/shared/context/MapsContext.tsx
+++ src/shared/context/MapsContext.tsx
@@ -24,6 +24,7 @@ export function MapsProvider({ children }: { children: ReactNode }) {
     );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useMaps() {
     return useContext(MapsContext);
 }
